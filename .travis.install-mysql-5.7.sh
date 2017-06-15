244f0604e609be5705e03c3c29c78c156eadd609
Alejandro <acarste@okstate.edu>
2017-06-15 20:42:56 +0000

    Trying to get Travis up and running.

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -7,4 +7,4 @@ echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo d
 wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
 sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
 sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
+sudo apt-get install -q --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
