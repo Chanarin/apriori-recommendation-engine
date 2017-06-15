973f539712fe68cebc48d27c0fb84c7db9e6981c
Alejandro <acarste@okstate.edu>
2017-06-15 21:11:42 +0000

    Trying to get Travis up and running.

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -4,11 +4,9 @@ sudo apt-get autoclean
 sudo rm -rf /var/lib/mysql
 sudo rm -rf /var/log/mysql
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
+wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
 sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
 sudo apt-get update -q
 sudo apt-get install -q --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
 sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
-sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
-sudo kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
 sudo service mysql restart
