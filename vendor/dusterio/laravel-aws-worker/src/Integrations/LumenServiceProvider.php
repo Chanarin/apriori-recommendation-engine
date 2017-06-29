a722b984e1ad98c64034b627ca8a33dcbdf202ed
Alejandro <acarste@okstate.edu>
2017-06-29 21:02:13 +0000

    Try to fix queueing problem.

--- vendor/dusterio/laravel-aws-worker/src/Integrations/LumenServiceProvider.php
+++ vendor/dusterio/laravel-aws-worker/src/Integrations/LumenServiceProvider.php
@@ -21,8 +21,7 @@ class LumenServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (function_exists('env') && ! env('REGISTER_WORKER_ROUTES', true)) return;
-
+        // if (function_exists('env') && ! env('REGISTER_WORKER_ROUTES', true)) return;
         $this->bindWorker();
         $this->addRoutes();
     }
@@ -32,8 +31,8 @@ class LumenServiceProvider extends ServiceProvider
      */
     protected function addRoutes()
     {
-        $this->app->post('/worker/schedule', 'Dusterio\AwsWorker\Controllers\WorkerController@schedule');
-        $this->app->post('/worker/queue', 'Dusterio\AwsWorker\Controllers\WorkerController@queue');
+        $this->app->post('/worker/schedule', '\Dusterio\AwsWorker\Controllers\WorkerController@schedule');
+        $this->app->post('/worker/queue', '\Dusterio\AwsWorker\Controllers\WorkerController@queue');
     }
 
     /**
