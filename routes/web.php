3f0e7ef14ebb3148ae7653c4c96998c9a144879c
Alejandro <acarste@okstate.edu>
2017-06-29 18:39:56 +0000

    Added queue routes.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ $app->get('/', function () {
     return view('index');
 });
 
-$app->post('/queue/worker', 'Dusterio\AwsWorker\Controllers\WorkerController@queue');
-$app->post('/queue/schedule', 'Dusterio\AwsWorker\Controllers\WorkerController@schedule');
+$app->post('/worker/queue', 'Dusterio\AwsWorker\Controllers\WorkerController@queue');
+$app->post('/worker/schedule', 'Dusterio\AwsWorker\Controllers\WorkerController@schedule');
 
 $app->group(['middleware' => 'throttle:250'], function () use ($app) {
 
