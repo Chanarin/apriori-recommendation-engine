50c80b42b95075be970fd1bd7357d5cdf548845a
Alejandro <acarste@okstate.edu>
2017-06-29 01:36:58 +0000

    Fixed trustedproxy configuration.

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -5,7 +5,7 @@ return [
     'proxies' => '*',
 
     'headers' => [
-        \Illuminate\Http\Request::HEADER_CLIENT_IP    => null,
+        \Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
         \Illuminate\Http\Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
         \Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
         \Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
